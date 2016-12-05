851f8a4c1b48772460426157770e3b7faf52580b
Jhon Olarte <jhonolarte@seti.com.co>
2016-12-05 11:10:44 -0500

    Refactor clase principal.

--- src/main/java/co/com/local/customer/application/CustomerApplication.java
+++ src/main/java/co/com/local/customer/application/CustomerApplication.java
@@ -23,37 +23,7 @@ public class CustomerApplication {
 
         jerseyServlet.setInitParameter(
                 "jersey.config.server.provider.classnames",
-                CustomerService.class.getCanonicalName());
-        
-	/*	ResourceConfig config = new ResourceConfig();
-		 config.packages("JavaGetCustomerRest");
-		 ServletHolder servlet = new ServletHolder(new ServletContainer(config));
-
-
-		Server server = new Server(2222);
-		 ServletContextHandler context = new ServletContextHandler(server, "/*");
-		 context.addServlet(servlet, "/*");
-*/
-
-		 
-        /*ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-
-        Server jettyServer = new Server(8070);
-        jettyServer.setHandler(context);
-
-        ServletHolder jerseyServlet = context.addServlet(
-             org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
-
-        // Tells the Jersey Servlet which REST service/class to load.
-        jerseyServlet.setInitParameter(
-           "jersey.config.server.provider.classnames",
-           CustomerService.class.getCanonicalName());
-        jerseyServlet.setInitParameter(
-                "co.com.local.customer.dto.CustomerDTO",
-                "true");
-*/
+                CustomerService.class.getCanonicalName());    
         try {
         	jettyServer.start();
         	jettyServer.join();
